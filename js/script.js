e522d2779664eacaac94c1778040968d53fe1020
Wojciech Nizialek <wojciech.nizialek@gmail.com>
2018-11-29 19:46:00 +0000

    Finish task

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function Phone(brand, price, color, battery) {
+	this.brand = brand;
+	this.price = price;
+    this.color = color;
+    this.battery = battery;
+};
+
+Phone.prototype.printInfo = function(){
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". Battery life: " + this.battery + " hours.");
+}
+
+var iPhone8 = new Phone("Apple", 2000, "grey", 10);
+var SamsungGalaxy = new Phone("Samsung", 2100, "black", 11);
+var HuaweiP20 = new Phone("Huawei", 2400, "black", 12);
+var HtcU12 = new Phone("HTC", 2000, "black", 13);
